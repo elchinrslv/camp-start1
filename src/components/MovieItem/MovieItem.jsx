227de8302e05e30579c12387621f20e3eebae03d
elchinrslv <elchinrslv@gmail.com>
2024-05-16 23:35:47 +0400

    save

---
+++ src/components/MovieItem/MovieItem.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./MovieItem.css";
+import { addFavoriteList } from "../../state/action";
+import { connect } from "react-redux";
+
+const MovieItem = ({ Title, Year, Poster, imdbID, favoriteList, addFavoriteList }) => {
+    const ifIdInFavorites = (imdbID) => {
+        const active = favoriteList.find((item) => {
+            return item.imdbID === imdbID;
+        });
+        if (active) {
+            return true;
+        }
+};
+
+    return (
+        <article className="movie-item">
+            <img className="movie-item__poster" src={Poster} alt={Title} />
+            <div className="movie-item__info">
+                <h3 className="movie-item__title">
+                    {Title} ({Year})
+                </h3>
+                <button type="button" className="movie-item__add-button" onClick={(e) => { addFavoriteList(imdbID);}} disabled={ifIdInFavorites(imdbID)} >
+                    Добавить в список
+                </button>
+            </div>
+        </article>
+    );
+}
+
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addFavoriteList: (imdbID) => {
+            dispatch(addFavoriteList(imdbID));
+
+        },
+    };
+};
+
+const mapStateToProps = (state) => {
+    return {
+        favoriteList: state.favoriteList,
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MovieItem);
